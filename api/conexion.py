10ac439f0fc30a57a7d19cba7c6d5d094254463f
sebaslazar <143653862+sebaslazar@users.noreply.github.com>
2023-09-05 13:56:34 -0500

    Update conexion.py

--- api/conexion.py
+++ api/conexion.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from sodapy import Socrata
 
 
@@ -15,4 +14,7 @@ def consultar_con_api(client, departamento, municipio, cultivo, limite):
     results_df = pd.DataFrame.from_records(results)
     results_df = results_df.filter(items=["departamento", "municipio", "cultivo", "topografia", "ph_agua_suelo_2_5_1_0",
                                           "f_sforo_p_bray_ii_mg_kg", "potasio_k_intercambiable_cmol_kg"])
-    return results_df
+    ph_mediana = pd.to_numeric(results_df["ph_agua_suelo_2_5_1_0"], errors='coerce').median()
+    fosforo_mediana = pd.to_numeric(results_df["f_sforo_p_bray_ii_mg_kg"], errors='coerce').median()
+    potasio_mediana = pd.to_numeric(results_df["potasio_k_intercambiable_cmol_kg"], errors='coerce').median()
+    return results_df, ph_mediana, fosforo_mediana, potasio_mediana
